89a1a7dea50e9541e20693930efaf5dac052b4c9
Marcelo <guedesmarcello733@gmail.com>
2021-07-08 12:57:56 -0300

    adicionando arquivo

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando Com Atribuição de Variáveis");
+const idade = 29;
+let nome = "Marcelo";
+const sobrenome = "Guedes"
+
+console.log(nome, sobrenome);
+console.log(nome + " " + sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+let contador = 0;
+contador = contador + 1;
