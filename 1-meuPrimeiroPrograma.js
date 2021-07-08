21206156f2cf149891fb5171426742cbbb9bb75c
Marcelo <guedesmarcello733@gmail.com>
2021-07-08 11:31:58 -0300

    Primeira Aula finalizada

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+//console.log imprime no terminal//
+console.log("Meu Primeiro Programa: Trabalhando com variáveis");
+
+//const é uma variavel//
+const idade=26;
+
+console.log(idade);
+console.log(idade+4);
+console.log(idade-4);
+console.log(idade/4);
+
+const idadeSomada = idade+6;
+console.log(idadeSomada)
