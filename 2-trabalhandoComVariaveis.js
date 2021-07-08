2c30882144453dba9ac43fc1ce4a90cae40a63d5
Marcelo <guedesmarcello733@gmail.com>
2021-07-08 12:03:26 -0300

    algumas atualizações

--- 2-trabalhandoComVariaveis.js
+++ 2-trabalhandoComVariaveis.js
@@ -5,4 +5,4 @@ const idade = 29;
 const nome = "Ricardo";
 
 //não declarar variavaveis sem o "const" memsmo com o java entendendo (pode ocorrer erros na hora de debbugar o codigo). Ex://
-mes = "Janeiro"
+//mes = "Janeiro";//
