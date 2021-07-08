646f334cfe884e76b9d8fb825e40ac986b92725f
Marcelo <84361418+Marcellowww@users.noreply.github.com>
2021-07-08 12:16:48 -0300

    Add files via upload

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando Com Variáveis");
+//JS é case sensitive ou seja letras maiusculas e minusculas fazem diferença//
+
+const idade = 29;
+const nome = "Ricardo";
+
+//não declarar variavaveis sem o "const" memsmo com o java entendendo (pode ocorrer erros na hora de debbugar o codigo). Ex://
+//mes = "Janeiro";//
