bad7ea08ef7d3d24d93fd767bee90fe5c55d7c21
Marcelo <guedesmarcello733@gmail.com>
2021-07-11 20:52:58 -0300

    atualizando o index

--- JavaScript II/index.js
+++ JavaScript II/index.js
@@ -1,22 +1,44 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
+}
 
+class ContaCorrente{
+    agencia;
+    #saldo = 0;
+
+    sacar(valor){
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
+            return valor
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+
+        this.#saldo += valor
+    }
+    
 }
 
 const cliente1 = new Cliente();
-const cliente2 =  new Cliente();
-
 cliente1.nome = "Marcelo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 =  new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteMarcelo = new ContaCorrente();
+contaCorrenteMarcelo.agencia = 1001;
+
+contaCorrenteMarcelo.depositar(100);
+
+const valorSacado = contaCorrenteMarcelo.sacar(50);
+console.log(valorSacado);
+
+
+console.log(contaCorrenteMarcelo);
